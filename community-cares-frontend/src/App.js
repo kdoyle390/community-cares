be78591e7cda1a4a2eda2e558eb145693572fb94
kdoyle390 <kdoyle390@gmail.com>
2023-02-01 18:31:56 -0600

    minor changes: added port and comment with basic fetch function to reuse

--- community-cares-frontend/src/App.js
+++ community-cares-frontend/src/App.js
@@ -4,6 +4,11 @@ import Search from './Search.js';
 import GoodContainer from './GoodContainer.js';
 
 function App() {
+// Update below with correct endpoint and add useEffect
+  // fetch('http://localhost:3006/')
+  // .then((response) => response.json())
+  // .then((data) => console.log(data));
+
   return (
     <div className="App">
       <h1 id="app-name">Community Cares</h1>
